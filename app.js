c7de9d9183dc15202d0a31dd757b9429fb2d2252
mankhan <mankhan@cs.stonybrook.edu>
2018-04-28 09:46:22 -0400

    Just made minor chnaged, and started switch from python to node.

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-// Using the additem that works.
+var express = require('express');
+var session = require('express-session');
+var multer  = require('multer');
+var path = require('path');
+var crypto = require('crypto');
+var cookieParser = require('cookie-parser');
+var fs = require('fs');
+const pg = require('pg');
+var nodemailer = require("nodemailer");
+const bodyParser = require('body-parser');
+const elasticsearch = require('elasticsearch');
 
+
+/** elastic search set up **/
+const INDEX_NAME = "insta_index"
+const DOC_TYPE="posts"
+
+
+var e_client = new elasticsearch.Client({
+  host: '130.245.171.41:9200',
+  log: 'trace'
+});
+
+/** --------------------- **/
+
+/** PostgreSQL setup **/
+//const conString = "postgres://postgres:" + encodeURIComponent("uB78#2xbut") + "@130.245.171.36:5432/instadata";
 var promise = require('bluebird');
 
 var options = {
@@ -11,118 +36,148 @@ var pgp = require('pg-promise')(options);
 var connectionString = "postgres://postgres:" + encodeURIComponent("uB78#2xbut") + "@130.245.171.36:5432/instadata";
 var db = pgp(connectionString);
 
+/** --------------------- **/
 
+/** media storage setup **/
+var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+    callback(null, '/var/www/media/media')
+    },
+    filename: function(req, file, callback) {
+    console.log("new_file")
+    console.log(file)
+    console.log("new file name")
+    console.log(new_filename);
+    var temp = file.originalname + '-' + Date.now() + path.extname(file.originalname);
+    var new_filename = crypto.createHash('md5').update(temp).digest('hex') + path.extname(file.originalname);
+    // console.log(new_filename);
+    callback(null, new_filename.toLowerCase());
+    }
+});
 
+/** --------------------- **/
+
+/** express setup **/
+var app = express()
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(session({secret: 'FbtEs4x32MEBN1EAaMpcDVpbAyGPpq'}));
+var user_session;
+
+/** add item start **/
 
 app.post('/additem', function (req, res) {
 
     console.log("for a post request on /additem");
-    logger.debug("for a post reuqest on /additem");
-    //var client = new pg.Client(conString);
-    // res.end();
 
-    var cookies = req.cookies;
-    // console.log(req.cookies);
-    // var data = req.body.data;
-    var data = req.body;
-    console.log(data);
-    logger.debug(data);
-
-
-    // logger.debug("disconnecting.");
-    var user_cookie = cookies.userID;
-    if(user_cookie == null) {
-        console.log("cookies : ", req.cookies);
-        return res.send(JSON.stringify({"status": "error", "error": "user is not logged in"}));
+    /** checking if user is logged in using sessions **/
+    user_session = req.session;
+    if(user_session == null) {
+        return res.json({status: "error", error: "User is not logged in"});
     }
+
+    console.log(data);
+
+    var user_id = user_session.userID;
+    if(user_id == null) {
+        console.log("cookies : ", req.cookies);
+        return res.json({status: "error", error: "User is not logged in"});
+    }
+    
+
+    /** defining variables to be used **/
     console.log("DEFINING VARS");
     var content = data.content == null ? null : data.content;
     var childType = data.childType == null ? null : data.childType;
     if (childType != null) {
         if (childType != "retweet" && childType != "reply") {
-            return res.send(JSON.stringify({status: "error", error: "Child type does not match requeired child type"}));
+            return res.json({status: "error", error: "Child type does not match requeired child type"});
         }
     }
-    if (content == null) {return res.send(JSON.stringify({status: "error", error: "content is null"}));}
+    if (content == null) {return res.json({status: "error", error: "content is null"});}
     var parent = data.parent == null ? null : data.parent;
     // var postid = data.postid;
-    var user_cookie = data.user_cookie;
+    // var user_cookie = data.user_cookie;
     if (parent == null && childType != null) {
-        return res.send(JSON.stringify({status: "error", error: "You cannot be a child without parent"}));
+        return res.json({status: "error", error: "You cannot be a child without parent"});
     }
-    console.log("starting to you know do things");
+    console.log("getting the postid for the posts");
     // Try updating the parents and everyhting. also do the connect.
-    postid = crypto.createHash('md5').update(Date.now().toString() + user_cookie).digest('hex')
+    // getting unique postid
+    var time = Date.now();
+    var random_num = Math.random()*time | 0;
+    var postid = crypto.createHash('md5').update(time.toString() + user_cookie + random_num.toString()).digest('hex');
+    
     try {
-    res.send(JSON.stringify({status: "OK", id: postid}));
-    if (parent != null) {
-        query = "INSERT INTO posts(username, postid, content, child_type, parent_id) VALUES ($1, $2, $3, $4, $5);"
+        res.json({status: "OK", id: postid});
+        if (parent != null) {
+            //query = "INSERT INTO posts(username, postid, content, child_type, parent_id) VALUES ($1, $2, $3, $4, $5);"
 
-        db.task("INSERT INTO posts(username, postid, content, child_type, parent_id) VALUES ($1, $2, $3, $4, $5);")
-        .then(function () {
-            //   res.status(200)
-            //     .json({
-            //       status: 'success',
-            //       message: 'yeee'
-            //     });
-            // })
-            console.log("ADDED NEW ITEM TO WITH PARENT ELE");
-
-
-
-            db.none("UPDATE posts set retweet_cnt = retweet_cnt+1 where postid=$1);", [parent])
+            db.task("INSERT INTO posts(username, postid, content, child_type, parent_id) VALUES ($1, $2, $3, $4, $5);")
             .then(function () {
+                //   res.status(200)
+                //     .json({
+                //       status: 'success',
+                //       message: 'yeee'
+                //     });
+                // })
                 console.log("ADDED NEW ITEM TO WITH PARENT ELE");
-            }) .catch(function (err) {
-                console.log("ERRR :(");
+
+
+
+                db.none("UPDATE posts set retweet_cnt = retweet_cnt+1 where postid=$1);", [parent])
+                .then(function () {
+                    console.log("ADDED NEW ITEM TO WITH PARENT ELE");
+                }) .catch(function (err) {
+                    console.log("ERRR :(");
+
+                    //return next(err);
+                });
+
+            })
+            .catch(function (err) {
+                console.log("ERRR :(",err);
 
                 //return next(err);
             });
 
-        })
-        .catch(function (err) {
-            console.log("ERRR :(",err);
-
-            //return next(err);
-        });
-
-    } else {
+        } else {
 
 
-        db.none("INSERT INTO posts(username, postid, content) VALUES ($1, $2, $3);",[user_cookie, postid, content])
-        .then(function () {
-            console.log("NO PARENT: ADDED NEW ITEM TO WITH PARENT ELE");
-        }).catch(function (err) {
-            console.log("ERRR :( bno pare", err);
-            //return next(err);
-        });
-    }
-
-    media = data.media;
-    if (media != null) {
-        for(var i = 0; i < media.length; i++) {
-        query = "INSERT INTO user_media (username, postid, mediaid) VALUES ($1, $2, $3);"
-
-        db.none("INSERT INTO user_media(username, postid, mediaid) VALUES ($1, $2, $3);",[user_cookie, postid, media[i]])
+            db.none("INSERT INTO posts(username, postid, content) VALUES ($1, $2, $3);",[user_cookie, postid, content])
             .then(function () {
-            // console.log("media done");
+                console.log("NO PARENT: ADDED NEW ITEM TO WITH PARENT ELE");
             }).catch(function (err) {
-                console.log("ERRR :( bno pare 0 media",err);
-            //return next(err);
+                console.log("ERRR :( bno pare", err);
+                //return next(err);
             });
-
         }
+
+        media = data.media;
+        if (media != null) {
+            for(var i = 0; i < media.length; i++) {
+            query = "INSERT INTO user_media (username, postid, mediaid) VALUES ($1, $2, $3);"
+
+            db.none("INSERT INTO user_media(username, postid, mediaid) VALUES ($1, $2, $3);",[user_cookie, postid, media[i]])
+                .then(function () {
+                // console.log("media done");
+                }).catch(function (err) {
+                    console.log("ERRR :( bno pare 0 media",err);
+                //return next(err);
+                });
+
+            }
+        }
+
+
+    } catch (e) {
+        // do logging
+        console.log(e.stack);
+        logger.debug(e.stack);
+        return;
+        // return res.send(JSON.stringify({"status": "error", "error": "DB connection problem."}));
     }
 
 
-    }
-    catch (e) {
-    // do logging
-    console.log(e.stack);
-    logger.debug(e.stack);
-    return;
-    // return res.send(JSON.stringify({"status": "error", "error": "DB connection problem."}));
-    }
+});
 
-
-})
